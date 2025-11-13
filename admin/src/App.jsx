4c473f23f803fc050d7e8803f0f9d03270c8c6db
Dakshta <dakshtasen2004@gmail.com>
2025-11-13 16:46:23 +0530

    Updated admin section

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,12 @@ import React, { useContext } from 'react'
 import Login from './pages/Login';
 import { ToastContainer, toast } from 'react-toastify';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
 
 const App = () => {
 
@@ -10,13 +16,24 @@ const App = () => {
 
 
   return aToken ? (
-    <div>
+    <div className='bg-[#F8F9FD]'>
      
       <ToastContainer />
+      <Navbar />
+      <div className='flex items-start'>
+      <Sidebar />
+     <Routes>
+      <Route path='/' element={<></>} />
+      <Route path='/admin-dashboard' element={<Dashboard />} />
+      <Route path='/all-appointments' element={<AllAppointments />} />
+      <Route path='/add-doctor' element={<AddDoctor />} />
+      <Route path='/doctor-list' element={<DoctorsList />} />
+     </Routes>
+      </div>
     </div>
   ) : (
     <>
-    <Login />
+      <Login />
       <ToastContainer />
     </>
   )
