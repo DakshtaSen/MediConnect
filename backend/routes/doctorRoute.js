7ce39160d0a4c244c15a03bbaf9d868264c5ea1e
utkarshi-chouhan <utkarshichouhan@email.com>
2025-12-03 22:08:23 +0530

    backend updated

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { doctorList } from '../controllers/doctorController.js'
-import doctorModel from '../models/doctorModel'
+import doctorModel from '../models/doctorModel.js'
 
 const doctorRouter = express.Router()
 
