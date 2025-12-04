e38171e0628b51f73407b6399f31e32f82cea874
Dakshta <dakshtasen2004@gmail.com>
2025-12-05 00:31:56 +0530

    updated login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,14 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
 
   const { backendUrl, token, setToken } = useContext(AppContext)
+
+  const navigate = useNavigate()
 
   const [state,setState] = useState('Sign Up')
 
@@ -39,6 +43,12 @@ const Login = () => {
     }
   }
 
+  useEffect(()=>{
+    if(token) {
+      navigate('/')
+    }
+  },[token])
+
 
   return (
     <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
@@ -52,17 +62,17 @@ const Login = () => {
         {
           state === "Sign Up" && <div className='w-full'>
           <p>Full Name</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='text' onChange={(e)=> setName(e.target.name)} value={name} required />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='text' onChange={(e)=> setName(e.target.value)} value={name} required />
         </div>
         }
         
         <div className='w-full'>
           <p>Email</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='email' onChange={(e)=> setEmail(e.target.name)} value={email} required />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='email' onChange={(e)=> setEmail(e.target.value)} value={email} required />
         </div>
         <div className='w-full'>
           <p>Password</p>
-          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='password' onChange={(e)=> setPassword(e.target.name)} value={password} required />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='password' onChange={(e)=> setPassword(e.target.value)} value={password} required />
         </div>
         <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
         {
